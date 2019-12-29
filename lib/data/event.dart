63ac713a52197b1311e4ebf1555428fd974c488d
Yiran Hu <yiranhu@gatech.edu>
2019-12-28 23:58:19 -0500

    Added some dummy JSON data and tested EventData.fromMap() constructor.

--- lib/data/event.dart
+++ lib/data/event.dart
@@ -8,16 +8,22 @@ class EventData {
     @required this.host,
     @required this.placeName,
     @required this.location,
-    @required this.time});
+    @required this.time,
+  });
 
   EventData.fromMap(Map<String, dynamic> map)
       : host = map['host'],
         placeName = map['place_name'],
-        location = LatLng(map['point']['coordinates'][1], map['point']['coordinates'][0]),
+        location = LatLng(map['location']['coordinates'][1], map['location']['coordinates'][0]),
         time = DateTime.parse(map['time']);
 
   final String host;
   final String placeName;
   final LatLng location;
   final DateTime time;
+
+  String toString() {
+    return host + ': ' + placeName + ' @ ' + time.toString();
+  }
+
 }
