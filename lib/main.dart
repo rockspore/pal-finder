67b287bafa3d785438b8815d4b9c1fe9ee8e1df1
Yiran Hu <yiranhu@gatech.edu>
2020-01-02 20:12:28 -0500

    Linked home screen to search screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-// import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:pal_finder/screens/home.dart';
 import 'package:pal_finder/screens/login.dart';
 import 'package:pal_finder/screens/place_search.dart';
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // print('Building MyApp');
-    return CupertinoApp(
+    return MaterialApp(
       title: _title,
       // navigatorKey: navigatorKey,
       initialRoute: '/',
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,24 +1,23 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:pal_finder/screens/place_search.dart';
+import 'package:pal_finder/widgets/event_list.dart';
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.home),
-          title: Text('Home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.search),
-          title: Text('Search'),
-        ),
-      ]),
-      tabBuilder: (context, index) {
-        return PlaceSearchScreen();
-      }
+    return Scaffold(
+      appBar: AppBar(
+        toolbarOpacity: 0.5,
+      ),
+      body: EventList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.pushNamed(context, '/place'),
+        tooltip: 'Add an Event',
+        child: Icon(Icons.add),
+      ),
     );
   }
+
 }
